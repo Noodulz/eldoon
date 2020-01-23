18576efed87c9128c0b11a1583a9d4ed788ce22a
Jocelyn Dzuong <jocelyndzuong@ufl.edu>
2020-01-23 00:45:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,33 @@
-# Still a work in progress to be able to scrape both saved posts and saved comments!!! Ahhh
 
 import praw
 import pandas as pd
-import datetime as dt
 
 usernamed = input("What's your username? ")
 passwrd = input("What's your password? ")
 
 reddit = praw.Reddit(client_id='', \
                     client_secret='', \
-                    user_agent='Eldoon', \
+                    user_agent='', \
                     username=usernamed, \
                     password=passwrd)
 
-savedpostsdict = {"saved title":[], \
-            "url":[]}
-savedcommentsdict = {"saved comments":[]}
+savedpostsdict = {"saved content url":[], \
+                  "saved content title":[]}
 
-savedposts = reddit.user.me().submissions.new(limit=None)
-savedcomments = reddit.redditor(usernamed).saved(limit=5)
+savedcommentsdict = {"saved comments": []}
 
-for submission in savedposts:
-    savedpostsdict["url"].append(submission.url)
-    savedpostsdict["saved title"].append(submission.title)
 
-for submission in savedcomments:
-    savedcommentsdict["saved comments"].append(submission.body)
+savedcontent = reddit.user.me().saved(limit=None)
+
+for submission in savedcontent:
+    if isinstance(submission, praw.models.Comment):
+        savedcommentsdict["saved comments"].append(submission.body)
+    elif isinstance(submission, praw.models.Submission):
+        savedpostsdict["saved content url"].append(submission.url)
+        savedpostsdict["saved content title"].append(submission.title)
+
 
 present_posts_data = pd.DataFrame(savedpostsdict)
 present_comments_data = pd.DataFrame(savedcommentsdict)
-present_posts_data.to_csv('savedredditposts.csv', index=False)
-present_comments_data.to_csv('savedcomments.csv', index=False)
+present_posts_data.to_csv('savedredditposts.csv', index=True)
+present_comments_data.to_csv('savedredditcomments.csv', index=True)
